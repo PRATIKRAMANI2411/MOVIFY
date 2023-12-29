b0043f4b7bf7b0d03042d6e84a04356ceeccb0b3
PRATIKRAMANI2411 <pratikramani22@gmail.com>
2023-12-29 23:25:36 +0530

    add populer and topreted movie

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import "./style.scss"
 import HeroBanner from './heroBanner/HeroBanner'
 import Tranding from './tranding/Tranding'
+import Populer from './populer/Populer'
+import TopRated from './topRated/TopRated'
 
 function Home() {
     return (
         <div className='homePage'>
             <HeroBanner />
-            <Tranding/>
+            <Tranding />
+            <Populer />
+            <TopRated />
         </div>
     )
 }
